d6fc768f56a7eed08b5d550216a3f61e61f6cf1d
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2020-05-10 16:50:59 +0200

    feat: add Mint & Burn events

--- contracts/T721Token.sol
+++ contracts/T721Token.sol
@@ -9,13 +9,18 @@ contract T721Token is IT721Token, ERC20, ERC20Detailed, Ownable {
 
     constructor() public ERC20Detailed("T721 Token", "T721T", 18) {}
 
+    event Mint(address indexed recipient, uint256 indexed amount);
+    event Burn(address indexed recipient, uint256 indexed amount);
+
     function mintFor(address _recipient, uint256 _amount) public onlyOwner {
         ERC20._mint(_recipient, _amount);
+        emit Mint(_recipient, _amount);
     }
 
     function burn(uint256 _amount) public {
         require(ERC20.balanceOf(msg.sender) >= _amount, "T721Token::burn | insufficient balance for burn");
         ERC20._burn(msg.sender, _amount);
+        emit Burn(msg.sender, _amount);
     }
 
 }
